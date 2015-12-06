b400a767a9d7d8364124eefcf84cf9aedade7534
Tina Sharma <tina_2212@hotmail.com>
2015-12-05 21:18:20 -0800

    css gulp file structure

--- tinasharma/gulpfile.js
+++ tinasharma/gulpfile.js
@@ -1,9 +1,29 @@
 var gulp = require('gulp');
 var webpack = require('webpack-stream');
+var minifyCss = require('gulp-minify-css');
+var concatCSS = require('gulp-concat-css');
+var gulpWatch = require('gulp-watch');
 
 gulp.task('static:dev',function() {
   gulp.src('app/**/*.html')
     .pipe(gulp.dest('build/'));
+});
+
+gulp.task('css:dev', function() {
+  return gulp.src([
+    'app/css/reset.css',
+    'app/css/base.css',
+    'app/css/layout.css',
+    'app/css/module.css',
+    'app/css/state.css',
+    'app/css/theme.css'])
+  .pipe(concatCSS('styles.min.css'))
+  .pipe(minifyCss())
+  .pipe(gulp.dest('build/'))
+});
+
+gulp.task('css:watch', function() {
+  gulp.watch('./app/css/**/*.css', ['css:dev']);
 });
 
 gulp.task('webpack:dev',function() {
@@ -26,5 +46,5 @@ gulp.task('webpack:test', function() {
   .pipe(gulp.dest('test/client/'));
 });
 
-gulp.task('build:dev', ['webpack:dev', 'static:dev']);
+gulp.task('build:dev', ['webpack:dev', 'static:dev', 'css:dev']);
 gulp.task('default', ['build:dev']);
