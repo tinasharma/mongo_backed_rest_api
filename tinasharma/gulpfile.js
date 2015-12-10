3d062fce6b1f1afe5dd35519df659fcf7bae7ddc
Tina Sharma <tina_2212@hotmail.com>
2015-12-09 22:16:22 -0800

    sass

--- tinasharma/gulpfile.js
+++ tinasharma/gulpfile.js
@@ -3,26 +3,37 @@ var webpack = require('webpack-stream');
 var minifyCss = require('gulp-minify-css');
 var concatCSS = require('gulp-concat-css');
 var gulpWatch = require('gulp-watch');
+var sass = require('gulp-sass');
+var maps = require('gulp-sourcemaps');
 
 gulp.task('static:dev',function() {
   gulp.src('app/**/*.html')
     .pipe(gulp.dest('build/'));
 });
 
-gulp.task('css:dev', function() {
-  return gulp.src([
-    'app/css/reset.css',
-    'app/css/base.css',
-    'app/css/layout.css',
-    'app/css/module.css',
-    'app/css/state.css'])
-  .pipe(concatCSS('styles.min.css'))
-  .pipe(minifyCss())
-  .pipe(gulp.dest('build/'))
+// gulp.task('css:dev', function() {
+//   return gulp.src([
+//     'app/css/reset.css',
+//     'app/css/base.css',
+//     'app/css/layout.css',
+//     'app/css/module.css',
+//     'app/css/state.css',])
+//   .pipe(concatCss('styles.min.css'))
+//   .pipe(minifyCss())
+//   .pipe(gulp.dest('build/'));
+// });
+
+gulp.task('sass:dev', function() {
+  gulp.src('./app/sass/**/*.scss') //gulp.src('./app/sass/**/*.scss')
+    .pipe(maps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(minifyCss())
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest('build/'));
 });
 
-gulp.task('css:watch', function() {
-  gulp.watch('./app/css/**/*.css', ['css:dev']);
+gulp.task('sass:watch', function() {
+  gulp.watch(['./app/sass/**/*.scss', './app/index.html'],['sass:dev', 'static:dev']);
 });
 
 gulp.task('webpack:dev',function() {
@@ -45,5 +56,5 @@ gulp.task('webpack:test', function() {
   .pipe(gulp.dest('test/client/'));
 });
 
-gulp.task('build:dev', ['webpack:dev', 'static:dev', 'css:dev']);
+gulp.task('build:dev', ['webpack:dev', 'static:dev', 'sass:dev']);
 gulp.task('default', ['build:dev']);
